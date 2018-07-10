758f1c92f738c15b4542f7aada040c59de135586
mazx <dyxdy@live.com>
2018-07-10 21:23:04 +0800

    pr

--- lab/lab2/PageRanker.java
+++ lab/lab2/PageRanker.java
@@ -1,7 +1,4 @@
 import java.io.IOException;
-import java.nio.charset.CharacterCodingException;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -22,122 +19,95 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class PageRanker {
-	public static class PageLinkMapper extends Mapper <Object, Text, Text, Text> {
+	public static class PageMapper extends Mapper <Object, Text, Text, Text> {
 		private Text keyInfo = new Text();
 		private Text valueInfo = new Text();
-		private static final Pattern linkPattern = Pattern.compile("\\[\\[.+?\\]\\]");
 
 		@Override
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			String[] strs = parseText(value);
-			if (!validPage(strs[0])) {
+			String str = value.toString();
+			int idx = str.indexOf("\t");
+			int idy = str.indexOf(";");
+			String page = str.substring(0, idx);
+			string rank = str.substring(idx + 1, idy);
+			string linkstr = str.substring(idy + 1);
+
+			keyInfo.set(page);
+			valueInfo.set("!");
+			context.write(keyInfo, valueInfo);
+
+			if (linkstr == "") {
+				return;
+			}
+			String[] links = linkstr.split(",");
+			for (String link : links) {
+				keyInfo.set(link);
+				valueInfo.set(page + ":" + rank + "," + links.length);
+				context.write(keyInfo, valueInfo);
+			}
+			keyInfo.set(page);
+			valueInfo.set("|" + linkstr);
+			context.write(keyInfo, valueInfo);
+		}
+	}
+	
+	public static class PageReducer extends Reducer <Text, Text, Text, Text> {
+		private Text res = new Text();
+		private static final float damping = 0.85F;
+
+		@Override
+		public void reduce(Text key, Iterable <Text> values, Context context) throws IOException, InterruptedException {
+			boolean flag = false;
+			String str;
+			String linkstr;
+			float tmp = 0;
+
+			for (Text value : Values) {
+				str = value.toString();
+				if (str.equals("!")) {
+					flag = true;
+					continue;
+				}
+				if (str.startsWith("|")) {
+					linkstr = str.substring(1);
+					continue;
+				}
+				int idx = str.indexOf(":");
+				int idy = str.indexOf(",");
+				float rank = Float.valueOf(str.substring(idx + 1, idy));
+				int cnt = Integer.valueOf(str.substring(idy + 1));
+				tmp += rank / cnt;
+			}
+
+			if (!flag) {
 				return;
 			}
 
-			keyInfo.set(strs[0].replace(" ", "_"));
-			Matcher matcher = linkPattern.matcher(strs[1]);
-			while (matcher.find()) {
-				String page = matcher.group();
-				page = formatPage(page);
-				if (page == null || page.isEmpty()) {
-					continue;
-				}
-				valueInfo.set(page);
-				context.write(keyInfo, valueInfo);
-			}
+			float rank = damping * tmp + (1 - damping);
+			res.set(String.valueOf(rank) + ";" + linkstr);
+			context.write(key, res)
 		}
-		
-		private String[] parseText(Text value) throws CharacterCodingException {
-			String[] strs = new String[2];
-			int l = value.find("<title>");
-			int r = value.find("</title>", l);
-			if (l == -1 || r == -1) {
-				return new String[] {"", ""};
-			}
-			l += 7;
-			strs[0] = Text.decode(value.getBytes(), l, r - l);
-			l = value.find("<text");
-			l = value.find(">", l) + 1;
-			r = value.find("</text>", l) - l;
-			if (l == -1 || r == -1) {
-				return new String[] {"", ""};
-			}
-			l += 1;
-			strs[1] = Text.decode(value.getBytes(), l, r - l);
-			return strs;
-		}
-
-		public boolean validPage(String str) {
-			return !str.contains(":");
-		}
-		
-		private String formatPage(String str) {
-			int l = 1;
-			if (str.startsWith("[[")); {
-				l = 2;
-			}
-
-			if (str.length() < l + 2 || str.length() > 100) {
-				return null;
-			}
-
-			char ch = str.charAt(l);
-			if (ch == '#' || ch == '.' || ch == '\'' || ch == '-' || ch == '{') {
-				return null;
-			}
-
-			if (str.contains(":") || str.contains(",") || str.contains("&")) {
-				return null;
-			}
-			int r = str.indexOf("]");
-			int t = str.indexOf("|");
-			if (t > 0) {
-				r = t;
-			}
-			t = str.indexOf("#");
-			if (t > 0) {
-				r = t;
-			}
-			str = str.substring(l, r);
-			str = str.replace(" ", "_");
-			return str;
-		}
-
-	}
-
-	public static class LinkReducer extends Reducer <Text, Text, Text, Text> {
-		private Text res = new Text();
-		
-		@Override
-		public void reduce(Text key, Iterable <Text> values, Context context) throws IOException, InterruptedException {
-			StringBuffer buf = new StringBuffer("1.0\t");
-			for (Text value : values) {
-				buf.append(value.toString() + ",");
-			}
-			res.set(buf.substring(0, buf.length() - 1));
-			context.write(key, res);
-		}
-	}
-
-	public static boolean parsePage() throws Exception {
-		Configuration conf = new Configuration();
-	
-		Job parseJob = Job.getInstance(conf, "parse page");
-		parseJob.setJarByClass(PageRanker.class);
-		
-		parseJob.setMapperClass(PageLinkMapper.class);
-		parseJob.setMapOutputKeyClass(Text.class);
-		parseJob.setMapOutputValueClass(Text.class);
-		parseJob.setReducerClass(LinkReducer.class);
-		parseJob.setOutputKeyClass(Text.class);
-		parseJob.setOutputValueClass(Text.class);
-
-		FileInputFormat.addInputPath(parseJob, new Path("/data/wiki-test"));
-		FileOutputFormat.setOutputPath(parseJob, new Path("/data/wiki-tmp/iter0"));
-		return parseJob.waitForCompletion(true);
 	}
 
 	public static void main(String[] args) throws Exception {
-		boolean flag = parsePage();
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "page rank");
+		job.setJarByClass(PageRank.class);
+		
+		job.setMapperClass(PageMapper.class);
+
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputKeyClass(Text.class);
+
+		job.setReducerClass(PageReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		
+		FileInputFormat.addInputPath(job, new Path("wiki-tmp/iter0"));
+		FileOutputFormat.setOutputPath(job, new Path("wiki-tmp/iter1"));
+		job.waitForCompletion(true);
+
+		System.exit(true);
 	}
 }
